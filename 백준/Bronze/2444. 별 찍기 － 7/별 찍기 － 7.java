a7ffc2ab273b06369b0513754050f34ddf9a58fd
경희 <101131146+jdaily00@users.noreply.github.com>
2025-01-22 12:58:30 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 76 ms, Memory: 12384 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,17 +9,26 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= N; i++) {
-            sb.append(" ".repeat(N - i));
-            sb.append("*".repeat(2 * i - 1));
+            sb.append(repeat(" ", N - i));
+            sb.append(repeat("*", 2 * i - 1));
             sb.append("\n");
         }
 
         for (int i = N - 1; i > 0; i--) {
-            sb.append(" ".repeat(N - i));
-            sb.append("*".repeat(2 * i - 1));
+            sb.append(repeat(" ", N - i));
+            sb.append(repeat("*", 2 * i - 1));
             sb.append("\n");
         }
 
         System.out.print(sb);
+    }
+
+    // 문자열 반복 메서드
+    private static String repeat(String str, int count) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            result.append(str);
+        }
+        return result.toString();
     }
 }
