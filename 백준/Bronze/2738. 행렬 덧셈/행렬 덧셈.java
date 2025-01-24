68a80fb7ce8ad668ef6c3dad5c4be86bb831b5be
경희 <101131146+jdaily00@users.noreply.github.com>
2025-01-24 16:03:56 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 100 ms, Memory: 13284 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,13 +1,15 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
         // 첫째 줄에 행렬의 크기 N과 M이 주어진다.
-        String[] size = br.readLine().split(" ");
-        int n = Integer.parseInt(size[0]);
-        int m = Integer.parseInt(size[1]);
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
         // 행렬 A와 B를 저장할 배열
         int[][] arr1 = new int[n][m];
@@ -15,17 +17,17 @@ public class Main {
 
         // 행렬 A 입력 받기
         for (int i = 0; i < n; i++) {
-            String[] row = br.readLine().split(" "); // 한 줄을 공백 기준으로 나누기
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr1[i][j] = Integer.parseInt(row[j]); // 정수로 변환하여 저장
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         // 행렬 B 입력 받기
         for (int i = 0; i < n; i++) {
-            String[] row = br.readLine().split(" "); // 한 줄을 공백 기준으로 나누기
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr2[i][j] = Integer.parseInt(row[j]); // 정수로 변환하여 저장
+                arr2[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -33,7 +35,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                sb.append(arr1[i][j] + arr2[i][j]).append(" "); // 두 행렬의 원소를 더해서 결과 저장
+                sb.append(arr1[i][j] + arr2[i][j]).append(" ");
             }
             sb.append("\n");
         }
